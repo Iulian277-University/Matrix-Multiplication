e013999fcee58360acd59bb13c694534c09e550a
iulian.taiatu <iulian.taiatu@nehalem-wn15.grid.pub.ro>
2023-04-30 12:21:23 +0300

    5.6 sec on N=1200

--- solver_opt.c
+++ solver_opt.c
@@ -8,6 +8,7 @@ void allocate_matrices(int N,
 						double **C,
 						double **BB,
 						double **BtBt,
+						double **BAt,
 						double **At,
 						double **Bt,
 						double **AB,
@@ -23,6 +24,10 @@ void allocate_matrices(int N,
 
 	*BtBt = calloc(N * N, sizeof(**BtBt));
 	if (*BtBt == NULL)
+		exit(EXIT_FAILURE);
+
+	*BAt = calloc(N * N, sizeof(**BAt));
+	if (*BAt == NULL)
 		exit(EXIT_FAILURE);
 
 	*At = calloc(N * N, sizeof(**At));
@@ -47,18 +52,20 @@ void allocate_matrices(int N,
  * C = A * B * A' + B' * B'
  */
 double* my_solver(int N, double *A, double* B) {
-	double *C, *BB, *BtBt, *At, *Bt, *AB, *ABAt;
-	allocate_matrices(N, &C, &BB, &BtBt, &At, &Bt, &AB, &ABAt);
+	double *C, *BB, *BtBt, *BAt,*At, *Bt, *AB, *ABAt;
+	allocate_matrices(N, &C, &BB, &BtBt, &BAt, &At, &Bt, &AB, &ABAt);
+	
+	////// Transpose A and B //////
 
 	// Compute At and Bt
-	for (register int i = 0; i != N; ++i) {
+	for (register int i = 0; i < N; ++i) {
 		register double *Aptr = A + i * N; // ith row of A
 		register double *Bptr = B + i * N; // ith row of B
 
 		register double *Atptr = At + i; // ith column of At
 		register double *Btptr = Bt + i; // ith column of Bt
 
-		for (register int j = 0; j != N; ++j) {
+		for (register int j = 0; j < N; ++j) {
 			*Atptr = *Aptr++;
 			*Btptr = *Bptr++;
 			Atptr += N;
@@ -66,26 +73,7 @@ double* my_solver(int N, double *A, double* B) {
 		}
 	}
 
-
-	// Compute B * At (store in C for now)
-	for (register int i = 0; i != N; ++i) {
-		register double *Cptr = C + i * N; // ith row of C
-		register double *Bcpy = B + i * N; // ith row of B
-
-		for (register int j = 0; j != N; ++j, ++Cptr) {
-			register double result = 0.0; // line of B * At
-
-			register double *Bptr = Bcpy + j;
-			register double *Aptr = A + j * (N + 1);
-
-			for (register int k = j; k < N; ++k, ++Bptr, ++Aptr)
-				result += *Bptr * *Aptr;
-
-			*Cptr = result;
-		}
-	}
-
-	// C for now is B * At
+	////// B'B' //////
 
 	// Compute BB = B * B; I will use Bt traversed on rows
 	for (register int i = 0; i < N; ++i) {
@@ -105,34 +93,65 @@ double* my_solver(int N, double *A, double* B) {
 		}
 	}
 
-	int i, j, k;
-	// Compute BB = B * B
-	// for (i = 0; i < N; ++i)
-	// 	for (k = 0; k < N; ++k)
-	// 		for (j = 0; j < N; ++j)
-	// 			BB[i * N + j] += B[i * N + k] * B[k * N + j];
-
 	// Compute BtBt = (BB)t
-	for (i = 0; i < N; ++i)
-		for (j = 0; j < N; ++j)
-			BtBt[i * N + j] = BB[j * N + i];
+	for (register int i = 0; i < N; ++i) {
+		register double *BBptr  = BB + i * N; // ith row of BB
+		register double *BtBtptr = BtBt + i; // ith column of BBt
+
+		for (register int j = 0; j < N; ++j) {
+			*BtBtptr = *BBptr++;
+			BtBtptr += N;
+		}
+	}
+
+	////// ABA' //////
 
 	// Compute AB = A * B
-	// for (i = 0; i < N; ++i)
-	// 	for (j = 0; j < N; ++j)
-	// 		for (k = i; k < N; ++k)
-	// 			AB[i * N + j] += A[i * N + k] * B[k * N + j];
+	for (register int i = 0; i < N; ++i) {
+		register double *ABptr = AB + i * N; // ith row of AB
+		register double *Acpy  = A  + i * N; // ith row of A
 
-	// Compute ABAt = A * BAt = A * C
-	for (i = 0; i < N; ++i)
-		for (j = 0; j < N; ++j)
-			for (k = i; k < N; ++k)
-				ABAt[i * N + j] += A[i * N + k] * C[k * N + j];
+		for (register int j = 0; j < N; ++j, ++ABptr) {
+			register double result = 0.0; // line of A * B
+
+			register double *Aptr = Acpy + i;
+			register double *Btptr = Bt + j * N + i; // jth row of Bt
+
+			for (register int k = i; k < N; ++k, ++Aptr, ++Btptr)
+				result += *Aptr * *Btptr;
+
+			*ABptr = result;
+		}
+	}
+
+	// Compute ABAt = AB * At
+	// At = A traversed on rows
+	for (register int i = 0; i < N; ++i) {
+		register double *ABAtPtr = ABAt + i * N; // ith row of ABAt
+		register double *ABptr = AB + i * N; // ith row of AB
+
+		for (register int j = 0; j < N; ++j, ++ABAtPtr) {
+			register double result = 0.0; // line of AB * At
+
+			register double *ABptr2 = ABptr + j;
+			register double *Atptr = A + j * (N + 1); // jth row of At
+
+			for (register int k = j; k < N; ++k, ++ABptr2, ++Atptr)
+				result += *ABptr2 * *Atptr;
+			
+			*ABAtPtr = result;
+		}
+	}
 
 	// Compute C = ABAt + BtBt
-	for (i = 0; i < N; ++i)
-		for (j = 0; j < N; ++j)
-			C[i * N + j] = ABAt[i * N + j] + BtBt[i * N + j];
+	for (register int i = 0; i < N; ++i) {
+		register double *Cptr = C + i * N; // ith row of C
+		register double *ABAtPtr = ABAt + i * N; // ith row of ABAt
+		register double *BtBtptr = BtBt + i * N; // ith row of BtBt
+
+		for (register int j = 0; j < N; ++j, ++Cptr, ++ABAtPtr, ++BtBtptr)
+			*Cptr = *ABAtPtr + *BtBtptr;
+	}
 
 	free(BB);
 	free(BtBt);
